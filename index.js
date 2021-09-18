08c435d3935d492045f01af5c58e839a191a4158
Jack Neo <vikasvarshney87@gmail.com>
2021-09-18 10:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const app = express()
-const port = 80
+const fs= require('fs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+fs.readFile("index.js","utf8",gotfile); // gotfile is callback function after reading the file 
+//fs.readFile("index.js","utf8",(err,data)=>{ console.log(data);});
 
-app.listen(port, () => { console.log("App is runing");});
+function gotfile(error,data){
+	if(error){ console.log(error);}
+	else{ console.log(data);}
+}
+console.log("File read done");
+
+
+var content=" This is a test file";
+fs.writeFile("myfile.txt",content,"utf8",filewritedone);
+function filewritedone(error){
+	if(error){ console.log(error);}
+	else{ console.log("File write done");}
+}
+
